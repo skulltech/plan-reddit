0e0571f77947ba222ab6ed022506f70457934ea5
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-11-30 19:47:59 +0530

    Add celery files

--- scheduler/views.py
+++ scheduler/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .forms import PostForm
 
 
 @login_required(login_url='/redditauth/authorize')
 def home(request):
-    pass
+    return
+
+@login_required(login_url='/redditauth/authorize')
+def create_post(request):
+    form = PostForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+
+    return render(request, 'create-post.html', {'form': form})
