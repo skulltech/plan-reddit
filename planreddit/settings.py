aca1fdcf36b362df15296e03917f79af2f61e079
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-11-30 19:12:34 +0530

    Create form and configure settings

--- planreddit/settings.py
+++ planreddit/settings.py
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
@@ -120,3 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Custom Settings
+
+AUTHENTICATION_BACKENDS = ['redditauth.backends.RedditBackend']
+AUTH_USER_MODEL = 'redditauth.RedditUser'
+SESSION_SAVE_EVERY_REQUEST = True
